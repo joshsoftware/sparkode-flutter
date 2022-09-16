1707f7a7d53336e16446e73ace3f16158a23f71d
Kaivalya Naik <kaivalya.naik@joshsoftware.com>
2022-09-17 01:02:40 +0530

    Home UI in progress

--- lib/utility/constants/strings.dart
+++ lib/utility/constants/strings.dart
@@ -11,4 +11,17 @@ class Strings {
   static const cancelText = "Cancel";
   static const errorText = "Error";
   static const noSearchResultsText = "No Search Results";
+
+  static const sparkCode = "Sparkode";
+  static const emailText = "Email";
+  static const passwordText = "Password";
+  static const enterEmailAndPassword = "Please enter email and password";
+  static const errors = "errors";
+  static const loginError = "Login Error";
+  static const loginText = "Login";
+  static const upcomingDrive = "Upcoming";
+  static const String ongoingDrive = "Ongoing";
+  static const completedDrive = "Completed";
+
+
 }
